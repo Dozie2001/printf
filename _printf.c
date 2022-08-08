069d00753dfe8bbe3571a3ed0b4e15a01ac3f31a
Asemota Francis <francisasemota.fa@gmail.com>
2022-08-08 09:23:35 -0700

    added the _printf.c file

--- _printf.c
+++ _printf.c
@@ -1,125 +1,46 @@
 #include "main.h"
-
 /**
- * print_str - prints a string
- * @format: formats in str
- * @count: current index
- * @args: argument list
- * @flag: flags array
- *
- * Return: number of characters printed
+ * _printf - is a function that selects the correct function to print.
+ * @format: identifier to look for.
+ * Return: the length of the string.
  */
-int print_str(const char *format, int count, va_list args, int *flag)
+int _printf(const char * const format, ...)
 {
-	char *s;
-	int retval;
+	convert_match m[] = {
+		{"%s", printf_string}, {"%c", printf_char},
+		{"%%", printf_37},
+		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
+		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
+		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
+		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
+	};
 
-	count = 0;
-	s = va_arg(args, char *);
-	if (s == NULL)
-		return (print_string("(nil)", flag, count));
-	s = (format[count] == 's') ? s : upper_str(s);
-	retval = print_string(s, flag, count);
-	/*free(s);*/
-	return (retval);
-}
+	va_list args;
+	int i = 0, j, len = 0;
 
-/**
- * print_int - prints a string
- * @format: formats in str
- * @count: current index
- * @args: argument list
- * @flag: flags array
- *
- * Return: number of characters printed
- */
-int print_int(const char *format, int count, va_list args, int *flag)
-{
-	long n;
-	char buffer[1024];
+	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
 
-	n = va_arg(args, int);
-	signedNumberToString(n, DECIMAL, buffer, format[count], flag);
-	return (print_string(buffer, flag, 1));
-
-}
-/**
- * isAlpha - check if a char is alphabet
- * @c: chracter to check
- *
- * Return: 0 if false 1 for true
- */
-int isAlpha(char c)
-{
-	return ((c <= 'z' && c >= 'a') ||
-			(c <= 'Z' && c >= 'A'));
-}
-
-/**
- * isDigit - check if a number is digit
- * @c: character to check
- *
- * Return: 1 for true 0 for false
- */
-int isDigit(char c)
-{
-	if (c < 48 || c > 58)
-		return (0);
-	return (1);
-}
-
-/**
- * setFlags - sets flags to represent the flg
- * Description: flags is an array of length 5
- * flags[0]: is set if (-) is found
- * flags[1]: is set if (+/spc) is found
- * flags[2]: is set if (0) is found
- * flags[3]: is set if (#) is found
- * flags[4]: is set if (digits) is found
- *
- * @flags: pointer to flags array
- * @fmt: pointer to format strings
- * @j: current index on fmt
- * @args: pointer to arguments list
- *
- * Return: number of chars checked
- */
-int setFlags(const char *fmt, int *flags, int j, va_list args)
-{
-	int i = j;
-
-	flags[0] = flags[1] = flags[2] = flags[3] = flags[4] = 0;
-	while ((!isDigit(fmt[i]) && !isAlpha(fmt[i])) || fmt[i] == '0')
+Here:
+	while (format[i] != '\0')
 	{
-		switch (fmt[i])
+		j = 13;
+		while (j >= 0)
 		{
-		case '-':
-			flags[0] = 1;
-			break;
-		case ' ':
-		case '+':
-			flags[1] = fmt[i];
-			break;
-		case '0':
-			flags[2] = 1;
-			break;
-		case '#':
-			flags[3] = 1;
-			break;
-		case '*':
-			flags[4] = va_arg(args, int);
-			break;
-		default:
-			break;
+			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+			{
+				len += m[j].f(args);
+				i = i + 2;
+				goto Here;
+			}
+			j--;
 		}
+		_putchar(format[i]);
+		len++;
 		i++;
 	}
-	if (isDigit(fmt[i]))
-		flags[4] = 0;
-	while (isDigit(fmt[i]))
-	{
-		flags[4] *= 10;
-		flags[4] += fmt[i++] - '0';
-	}
-	return (i - j);
+	va_end(args);
+	return (len);
 }
+
